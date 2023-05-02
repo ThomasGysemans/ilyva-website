7c85968a48655ea9c957cf381f961181c4dae110
ThomasGysemans <support@sciencesky.fr>
2023-05-02 10:18:07 +0200

    Update redirections to include 'trello'

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { Handle } from "@sveltejs/kit";
 import redirect from "$lib/server/redirect";
 
 export const handle: Handle = async ({ resolve, event }) => {  
-  if (event.url.pathname === "/notion") {
+  if (event.url.pathname === "/notion" || event.url.pathname === "/trello") {
     return redirect("https://ilyva.notion.site/88b5c7f2f8e1423a8e817f3fe3abfc30?v=33a6902f1da842798b3c2d26d8ac519b", 301);
   } else if (event.url.pathname === "/game") {
     return redirect("https://yoshiip.xyz/ilyva/html");
